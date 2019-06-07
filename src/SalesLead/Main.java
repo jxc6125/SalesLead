ba953d233ee086a8b56ab48b52bf109b8bc91a55
jxc6125 <Esmeralda@owners-MacBook-Pro.local>
2019-06-07 13:06:29 -0400

    Initial commit

---
+++ src/SalesLead/Main.java
@@ -0,0 +1,19 @@
+package SalesLead;
+
+
+
+        public class Employees {
+            private String IDnumber;
+            private String Qualifications;
+            private String Access to Car Inventory;
+            private String Recent Sales;
+        }
+        public String getIDnumber() {
+        }
+        public String get Qualifications() {
+            Years of experience
+        }
+        public String Sale History(){
+            Access to every employee’s sales history }
+        Public class Car() (Access to car Inventory, Orion’s portion    }
+}
